b6480b82550836a91eb6d2c5ea2274bd1af91e4d
Jordan Cazamias <jacazamias@gmail.com>
2015-03-23 22:35:47 -0500

    Add test bash script to run omp and store data

--- testomp.sh
+++ testomp.sh
@@ -1,23 +1,28 @@
 #!/bin/bash
 
 maxnums="512"
-startnum="512"
+startnum="1"
 maxthreads="4"
-thread="4"
+startthread="1"
 iters="50"
+
+writefile=$1
 
 cat /dev/null > test.txt
 
 # Perform tests up to maxthreads and maxnums
+thread=$startthread
 while [ $thread -le $maxthreads ]
 do
-	num="1"
+	num=$startnum
 	while [ $num -le $maxnums ]
-	do
-		./psum_openmp $num $thread $iters >> test.txt
+    do
+        printf "%d threads, %d nums\n" $thread $num
+		time=`./psum_openmp $thread $num $iters`
+        printf "%f\t" $time >> $writefile
 		wait
 		num=$[$num*2]
 	done
 	thread=$[$thread+1]
-	printf "\n" >> test.txt
+	printf "\n" >> $writefile
 done
